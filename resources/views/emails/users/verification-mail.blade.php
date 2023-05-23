975e4e991dd54de16f16096823f09da83b8c20fe
TychaK <kelvinkibugi@gmail.com>
2023-05-24 00:06:27 +0300

    Added Verification email view on successful registration.

---
+++ resources/views/emails/users/verification-mail.blade.php
@@ -0,0 +1,6 @@
+<div>
+    Hello {{$user->name}}, thank you for joining {{env('APP_NAME')}}.
+    To verify your email, please click on {{$link}}.
+    Thank you.
+    {{env('app_name')}} Team.
+</div>
