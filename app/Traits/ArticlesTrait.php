d2fcf3b4cb7f52e2e815d20e50bfacc1dbfbad4a
TychaK <kelvinkibugi@gmail.com>
2023-05-24 00:08:50 +0300

    Added trait fetch to be used to make API calls

--- app/Traits/ArticlesTrait.php
+++ app/Traits/ArticlesTrait.php
@@ -2,7 +2,38 @@
 
 namespace App\Traits;
 
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+
 trait ArticlesTrait
 {
+    public function fetch($url)
+    {
 
+        $client = new Client();
+
+        try {
+
+            $response = $client->request('GET', $url, [
+                'headers' => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+
+            $responseData = json_decode(($response->getBody()->getContents()));
+
+            Log::info("Received response from api call  ... " . json_encode($responseData));
+
+            return $responseData;
+
+        } catch (\GuzzleHttp\Exception\RequestException $exception) {
+
+            Log::info("Encountered exception trying to get data ...");
+
+            Log::info($exception->getResponse()->getBody());
+
+            return [];
+        }
+
+    }
 }
