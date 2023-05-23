de3df4e026cdaaf56c542e0fd73c647faf78768f
TychaK <kelvinkibugi@gmail.com>
2023-05-24 00:09:08 +0300

    Added articles routes, added login routes and registration routes.

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function () {
+    Route::resource('/articles', 'ArticleController');
+});
+
+Route::group(['namespace' => 'Auth'], function () {
+    Route::post('/login', 'LoginController@login');
+    Route::post('/register', 'RegisterController@register');
 });
