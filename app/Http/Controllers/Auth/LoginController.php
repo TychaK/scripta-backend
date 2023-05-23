35fe038c64e7683736e3d7c5a6b03491a184ce06
TychaK <kelvinkibugi@gmail.com>
2023-05-24 00:12:11 +0300

    Added Login Controller for auth / login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    //
+
+    public function login(Request $request): JsonResponse
+    {
+
+        $user = User::where('email', $request->input('email'))
+            ->first();
+
+        if ($user) {
+            if ((Hash::check($request->password, $user->password))) {
+                Auth::login($user);
+                $token = $user->createToken('scripta')->accessToken;
+                $user->token = $token;
+                return response()->json(
+                    [
+                        'message' => 'Login successful.',
+                        'user' => $user
+                    ],
+                    Response::HTTP_OK);
+            } else {
+                return response()->json(
+                    [
+                        'message' => 'Credentials Provided are Incorrect'
+                    ],
+                    Response::HTTP_UNAUTHORIZED);
+            }
+        } else {
+            return response()->json(
+                [
+                    'message' => 'Credentials Provided are Incorrect'
+                ],
+                Response::HTTP_UNAUTHORIZED);
+        }
+
+    }
+
+}
