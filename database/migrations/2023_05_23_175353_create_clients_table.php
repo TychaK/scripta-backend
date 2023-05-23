f3eb3eaf58e7cca9e59b32244758e16f05c6ab45
TychaK <kelvinkibugi@gmail.com>
2023-05-24 00:10:04 +0300

    Added clients migration for creating clients table.

---
+++ database/migrations/2023_05_23_175353_create_clients_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('name');
+            $table->string('slug');
+            $table->string('base_url');
+            $table->string('api_key');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
