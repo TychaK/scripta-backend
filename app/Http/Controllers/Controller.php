bcbc5db531752b90f89806566ec3413951ed94b0
TychaK <kelvinkibugi@gmail.com>
2023-05-28 16:19:14 +0300

    Added convenient method parseQuery way to either paginate / get results without pagination from db.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function parseQuery($query, Request $request)
+    {
+        if (isset($request->paginate)) {
+            return $query->paginate($request->input('per_page', 25));
+        } else {
+            return $query->get();
+        }
+    }
+
 }
